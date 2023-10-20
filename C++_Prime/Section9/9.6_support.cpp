2260f53b997ee5528caa12c309458922e8915bf6
Ji Won Choi <jiwonchoi3794@gmail.com>
2023-10-21 00:05:14 +0900

    외부 링크

--- C++_Prime/Section9/9.6_support.cpp
+++ C++_Prime/Section9/9.6_support.cpp
@@ -13,4 +13,17 @@ void update(double dt)  // 전역 변수를 갱신한다
 {
     extern double warming;  // 선택적 재선언
     warming += dt;      // 전역 warming 을 사용한다
+    cout << "전역 warming이 " << warming;
+    cout << "도로 갱신되었습니다. \n";
+}
+
+void local() // 지역 변수를 사용한다
+{
+    double warming = 0.8;       // 새 변수로 외부 변수 warming을 가린다
+
+    cout << "지역 warming은 " << warming << "도입니다.\n";
+        // 사용 범위 결정 연산자를 사용하여
+        // 전역 변수에 접근한다
+    cout << "그러나, 전역 warming은 " << ::warming;
+    cout << "도입니다. \n";
 }
