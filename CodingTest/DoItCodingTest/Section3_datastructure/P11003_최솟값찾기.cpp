bfe834c90796d27340be4156951da40b353b04bc
Ji Won Choi <jiwonchoi3794@gmail.com>
2023-11-08 19:36:58 +0900

    3.4 슬라이딩 윈도우 _ 최솟값 찾기1

---
+++ CodingTest/DoItCodingTest/Section3_datastructure/P11003_최솟값찾기.cpp
@@ -0,0 +1,36 @@
+// 자료구조/P11003_최솟값찾기.cpp
+#include <iostream>
+#include <deque>
+using namespace std;
+typedef pair<int, int> Node;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, L;
+    cin >> N >> L;
+    deque<Node> mydeque;
+
+    for (int i = 0; i < N; i++) {
+        int now;
+        cin >> now;
+        // 값이 들오올 때마다 정렬하지 않고
+        // 현재 수 보다 큰 값을 덱에서 제거하여 시간 복잡도를 줄임
+        while (mydeque.size() && mydeque.back().first > now) {
+            mydeque.pop_back();
+        }
+        mydeque.push_back(Node(now, i));
+        // 범위에서 벗어난 값은 덱에서 제거
+        if(mydeque.front().second <= i - L) {
+            mydeque.pop_front();
+        }
+        cout << mydeque.front().first << " ";
+    }
+
+
+    return 0;
+}
